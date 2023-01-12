e5dfa742b162c0e7e8f67f94b2167adeff1b4f01
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-01-12 12:44:59 -0400

    user_model created

--- database/config/config.js
+++ database/config/config.js
@@ -1,53 +1,53 @@
 "use strict";
 require('dotenv').config();
 module.exports = {
-  development: {
-    use_env_variable: 'DATABASE_URI_DEV',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-      deletedAt: 'deleted_at',
-    },
-    dialectOptions: {
-      useUTC: true,
-    },
-    timezone: 'UTC'
+    development: {
+      use_env_variable: 'DATABASE_URI_DEV',
+      define: {
+          timestamps: true,
+          underscored: true,
+          underscoredAll: true,
+          createdAt:'created_at',
+          updatedAt:'updated_at',
+          deletedAt:'deleted_at',
+      },
+      dialectOptions: {
+        useUTC: true,
+      },
+      timezone: 'UTC'
   },
   test: {
-    use_env_variable: 'DATABASE_URI_TEST',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-      deletedAt: 'deleted_at',
+      use_env_variable: 'DATABASE_URI_TEST',
+      define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+        createdAt:'created_at',
+        updatedAt:'updated_at',
+        deletedAt:'deleted_at',
     },
     dialectOptions: {
-      useUTC: true,
+        useUTC: true,
     },
-    timezone: 'UTC'
+      timezone:'UTC'
   },
   production: {
-    use_env_variable: 'DATABASE_URI_PROD',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
+      use_env_variable: 'DATABASE_URI_PROD',
+      dialectOptions: {
+          ssl: {
+              require: true,
+              rejectUnauthorized: false,
+          },
+          useUTC: true,
       },
-      useUTC: true,
-    },
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-      deletedAt: 'deleted_at',
-    },
-    timezone: 'UTC'
+      define: {
+          timestamps: true,
+          underscored: true,
+          underscoredAll: true,
+          createdAt:'created_at',
+          updatedAt:'updated_at',
+          deletedAt:'deleted_at',
+      },
+      timezone:'UTC'
   }
-};
+}

72374464954d9ce8a71346728a4c59ba1019c94b
Aldo Ricardo Reygadas Hernandez <aldo@hobbio.co>
2023-01-17 15:44:09 -0600

    feat: countries service, model, controller working

--- utils/models.router.js
+++ utils/models.router.js
@@ -4,6 +4,7 @@ const routesPublications = require('../src/routes/publications_types.routes');
 const routesCities = require('../src/routes/city.routes');
 const routesVotes = require('../src/routes/vote.routes');
 const routesRoles = require('../src/routes/role.routes');
+const routesCountries = require('../src/routes/country.routes');
 
 function routerModels(app) {
   const router = express.Router();
@@ -14,6 +15,7 @@ function routerModels(app) {
   router.use('/cities', routesCities);
   router.use('/votes', routesVotes);
   router.use('/roles', routesRoles);
+  router.use('/countries', routesCountries);
 
   // other models here
 }

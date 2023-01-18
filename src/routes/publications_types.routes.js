36b7d63219106492540cce92af1cabacd394f7f8
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-01-18 00:45:28 -0400

    problem with publications_types fixed

--- src/routes/publications_types.routes.js
+++ src/routes/publications_types.routes.js
@@ -2,16 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 const {
-    getPublications,
-    addPublication,
-    getPublication,
-    updatePublication,
-    removePublication } = require('../services/publications_types.services')
+    getPublicationsTypes,
+    addPublicationTypes,
+    getPublicationTypes,
+    updatePublicationTypes,
+    removePublicationTypes } = require('../controllers/publications_types.controllers')
 
-router.get('/', getPublications)
-router.post('/', addPublication)
-router.get('/:id', getPublication)
-router.put('/:id', updatePublication)
-router.delete('/:id', removePublication)
+router.get('/', getPublicationsTypes)
+router.post('/', addPublicationTypes)
+router.get('/:id', getPublicationTypes)
+router.put('/:id', updatePublicationTypes)
+router.delete('/:id', removePublicationTypes)
 
 module.exports = router

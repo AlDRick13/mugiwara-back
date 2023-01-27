ef06f7330cb3e92659cc21e1eba0389fc131de11
TonyOP46 <88261670+TonyOP46@users.noreply.github.com>
2023-01-27 16:15:31 -0500

    feat: Swagger-created!

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,6 +10,37 @@ const {
   updateUser,
   removeUser } = require('../controllers/user.controllers');
 
+/**
+ * @openapi
+ * /api/v1/auth/signup:
+ *   post:
+ *     summary: Register a new user into the app
+ *     tags: [Users]
+ *     requestBody:
+ *       description: To register a new user you need a some parameters
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/register"
+ *     responses:
+ *       201:
+ *         description: created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/users"
+ */
+
+
 router.get('/', passportJWT.authenticate('jwt', { session: false }), getUsers);
 // router.post('/', addUser) auth/login
 router.get('/:id', passportJWT.authenticate('jwt', { session: false }), getUser);

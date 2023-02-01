fac88f8fddb8868cb2b30770ba1db2381f6a9248
TonyOP46 <88261670+TonyOP46@users.noreply.github.com>
2023-01-31 23:38:43 -0500

    feat: Swagger

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,5 +1,94 @@
 /**
  * @openapi
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:            
+ *       type: apiKey
+ *       in: header
+ *       name: Authorization
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   schemas:
+ *     users:
+ *       type: object
+ *       properties:
+ *         first_name:
+ *           type: string
+ *           example: Tony
+ *         last_name:
+ *           type: string
+ *           example: Ospino
+ *         email:
+ *           type: string
+ *           example: tony@gmail.com
+ *     register:
+ *       type: object
+ *       properties:
+ *         first_name:
+ *           type: string
+ *           example: Tony
+ *         last_name:
+ *           type: string
+ *           example: Ospino
+ *         email:
+ *           type: string
+ *           example: tony@gmail.com
+ *         username:
+ *           type: string
+ *           example: Tonyop46
+ *         password:
+ *           type: string
+ *           example: 1234
+ *         email_verified:
+ *           type: Date
+ *           example: 2023/01/30
+ *         token:
+ *           type: string
+ *           example: 12dfdf2232dfdf
+ *         role_id:
+ *           type: int
+ *           example: 1
+ *         image_url:
+ *           type: string
+ *           example: "Hola chula"
+ *         code_phone:
+ *           type: string
+ *           example: "111121111"
+ *         phone:
+ *           type: string
+ *           example: "88889288823382"
+ *         country_id:
+ *           type: int
+ *           example: 1
+ * 
+ *  
+ */
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     user:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *           example: tony@gmail.com
+ *         password:
+ *           type: string
+ *           example: 1234 
+ *     login:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *           example: tony@gmail.com  
+ * 
+ */
+
+
+/**
+ * @openapi
  * /api/v1/auth/signup:
  *   post:
  *     summary: Register a new user into the app
@@ -60,6 +149,31 @@
  *                     $ref: "#/components/schemas/login"
  *     
  *
+ */
+
+/**
+ * @openapi
+ * /api/v1/auth/user-info:
+ *   get:
+ *     summary: Get user information
+ *     tags: [Auth. Register and Login]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: This is your user information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
  */
 
 const express = require('express');
